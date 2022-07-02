863748012d5ec12da92eb365eb27d43cb0983321
kaveenSP <kaveen.premachandra@gmail.com>
2022-07-02 16:43:37 +0530

    interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,18 @@ import { AppComponent } from './app.component';
 import { CustomersComponent } from './customers/customers.component';
 import { CustomersListComponent } from './customers/customers-list/customers-list.component';
 import { FilterTextboxComponent } from './customers/customers-list/filter-textbox.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomersComponent,
     CustomersListComponent,
-    FilterTextboxComponent
+    FilterTextboxComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
